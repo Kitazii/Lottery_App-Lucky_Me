c7b54f569dd003201bfa21bfd6c1d40c04dbe8a6
mrkie <mrkie@DESKTOP-04STFUU>
2023-11-02 00:37:45 +0000

    Add project files.

---
+++ LuckyMeMVC/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using LuckyMeMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LuckyMeMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //this action returns the empty form
+        [HttpGet]
+        public ActionResult CheckNumbers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckNumbers(LotteryNumber model)
+        {
+            //This list var will hold the value of the weekly lottery draw (six int values)
+            List<int> weeklyDraw = new List<int>() { 23, 27, 34, 39, 40, 3 };
+
+            //the counter will hold how many of the user's nums matches the weekly lottery draw
+            int counter = 0;
+
+            //looping from the weekly lottery draw to count how many numbers match
+            //if theres a single match, we increment the counter by one
+            foreach (int lotter_num in weeklyDraw)
+            {
+                if (lotter_num == model.Number_1)
+                {
+                    counter++;
+                }
+                else if (lotter_num == model.Number_2)
+                {
+                    counter++;
+                }
+                else if (lotter_num == model.Number_3)
+                {
+                    counter++;
+                }
+                else if (lotter_num == model.Number_4)
+                {
+                    counter++;
+                }
+                else if (lotter_num == model.Number_5)
+                {
+                    counter++;
+                }
+                else if (lotter_num == model.Number_6)
+                {
+                    counter++;
+                }
+            } //end of foreach
+
+            //checking if the user has 6 matching nnumber then he wins the jackpot else...
+            if (counter == 6)
+            {
+                ViewBag.Winning = "Congratulations! You won 10 Million";
+            }
+            else
+            {
+                ViewBag.Losing = "Sorry maybe you have better luck next time!";
+            }
+
+            return View();
+        }//end of CheckNumbers Method
+    }//end of HomeController class
+}//end of namespace
---
+++ LuckyMeMVC/Models/LotteryNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LuckyMeMVC.Models
+{
+    public class LotteryNumber
+    {
+        [Required]
+        [Display(Name = "Number 1")]
+        [Range(1, 59, ErrorMessage = "Please enter a positive whole number between 1 and 59")]
+        public int Number_1 { get; set; }
+
+        [Required]
+        [Display(Name = "Number 2")]
+        [Range(1, 59, ErrorMessage = "Please enter a positive whole number between 1 and 59")]
+        public int Number_2 { get; set; }
+
+        [Required]
+        [Display(Name = "Number 3")]
+        [Range(1, 59, ErrorMessage = "Please enter a positive whole number between 1 and 59")]
+        public int Number_3 { get; set; }
+
+        [Required]
+        [Display(Name = "Number 4")]
+        [Range(1, 59, ErrorMessage = "Please enter a positive whole number between 1 and 59")]
+        public int Number_4 { get; set; }
+
+        [Required]
+        [Display(Name = "Number 5")]
+        [Range(1, 59, ErrorMessage = "Please enter a positive whole number between 1 and 59")]
+        public int Number_5 { get; set; }
+
+        [Required]
+        [Display(Name = "Number 6")]
+        [Range(1, 59, ErrorMessage = "Please enter a positive whole number between 1 and 59")]
+        public int Number_6 { get; set; }
+
+    }
+}
